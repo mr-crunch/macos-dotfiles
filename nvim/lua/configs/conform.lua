73b79a714c58fd11798cca3727808143bac2147d
mr-crunch <mattbianco4@gmail.com>
2025-01-25 02:42:17 -0600

    changed clang-format to LLVM

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -11,7 +11,7 @@ local options = {
 		["clang-format"] = {
 			prepend_args = {
 				"-style={ \
-        BasedOnStyle: Microsoft, \
+        BasedOnStyle: LLVM, \
         IndentWidth: 2,\
         TabWidth: 2,\
         UseTab: Never,\
